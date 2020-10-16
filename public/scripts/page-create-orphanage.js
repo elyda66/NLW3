bb83918580e1213a0bb5bed439e1cf9207aa7a0f
Elyda Rodrigues <elydarodrigues66@gmail.com>
2020-10-16 18:09:21 -0300

    Dias 2 a 3

--- public/scripts/page-create-orphanage.js
+++ public/scripts/page-create-orphanage.js
@@ -15,9 +15,11 @@ const icon = L.icon({
 let marker;
 
 map.on("click", (event) => {
-    console.log(event)
   const lat = event.latlng.lat;
   const lng = event.latlng.lng;
+
+  document.querySelector('[name=lat]').value = lat;
+  document.querySelector('[name=lng]').value = lng;
 
     //REMOVE ICON AFTER ADD 
     marker && map.removeLayer(marker)
@@ -26,3 +28,47 @@ map.on("click", (event) => {
   marker = L.marker([lat, lng], { icon })
   .addTo(map)
 })
+
+//SELECT PHOTOS
+
+function addPhotoField () {
+    //PEGAR O CONTEINER DE FOTOS
+    const conteiner = document.querySelector('#images')
+
+    //PEGAR O CONTEINER PARA DUPLICAR 
+    const fielsdConteiner = document.querySelectorAll('.new-upload')
+    
+    //REALIZAR O CLONE DA ÚLTIMA IMAGEM ADICIONADA
+    const newfieldconteiner = fielsdConteiner[fielsdConteiner.length -1].cloneNode(true);
+    
+    // VERIFICAR SE ESTA VAZIO 
+    const input = newfieldconteiner.children[0]
+
+    if(input.value == ""){
+      return
+    }
+
+    //LIMPAR O CAMPO ANTES 
+    input.value = ""
+
+    //ADICIONAR O CLONE AO CONTEINER DE IMAGENS
+    conteiner.appendChild(newfieldconteiner)
+
+}
+
+function deletefield (event) {
+  const span = event.currentTarget
+
+  const fielsdConteiner = document.querySelectorAll('.new-upload')
+
+  if(fielsdConteiner.length <= 1) {
+    // lIMPAR O  VALOR DO CAMPO
+    span.parentNode.children[0].value = ""
+    return
+  }
+
+  // DELETAR O CAMPO 
+  span.parentNode.remove();
+
+
+}
